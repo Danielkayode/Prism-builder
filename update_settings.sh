e8c5c456ef9c4dc479405f46cec8574e2125053c
Danielkayode <153762458+Danielkayode@users.noreply.github.com>
2026-01-08 21:56:01 +0100

    Update update_settings.sh

--- update_settings.sh
+++ update_settings.sh
@@ -7,12 +7,11 @@
 echo "Injecting Prism-specific configurations..."
 URL="https://github.com/${GH_REPO_PATH}/releases/download/${RELEASE_VERSION}/${APP_NAME_LC}-reh-\${os}-\${arch}-${RELEASE_VERSION}.tar.gz"
 
-# 1. Inject the serverDownloadUrlTemplate (replaces add-remote-url.patch)
+# 1. Inject the serverDownloadUrlTemplate
 sed -i "s@version }))@version, serverDownloadUrlTemplate: '${URL}' })@g" build/gulpfile.reh.js
 sed -i "s@version }))@version, serverDownloadUrlTemplate: '${URL}' })@g" build/gulpfile.vscode.js
 
-# 2. Inject Binary Name (replaces binary-name.patch)
-# Targets the name property in the packaging task
+# 2. Inject Binary Name
 sed -i "s/name: 'code-oss'/name: '${BINARY_NAME}'/g" build/gulpfile.vscode.js
 
 echo "Update settings completed successfully."

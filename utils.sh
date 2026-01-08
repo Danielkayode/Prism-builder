858f4e8eaf2937ea13a75f691251a5c142660c3c
Danielkayode <153762458+Danielkayode@users.noreply.github.com>
2026-01-08 01:58:55 +0100

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-APP_NAME="${APP_NAME:-Void}"
-APP_NAME_LC="$( echo "${APP_NAME}" | awk '{print tolower($0)}' )"
-BINARY_NAME="${BINARY_NAME:-void}"
-GH_REPO_PATH="${GH_REPO_PATH:-voideditor/void}"
-ORG_NAME="${ORG_NAME:-voideditor}"
+# Application name and branding
+APP_NAME="${APP_NAME:-Prism}"
+APP_NAME_LC=$( echo "${APP_NAME}" | awk '{print tolower($0)}' )
+BINARY_NAME="${BINARY_NAME:-prism}"
+
+# GitHub configuration
+GH_REPO_PATH="${GH_REPO_PATH:-Danielkayode/binaries}"
+ORG_NAME="${ORG_NAME:-Danielkayode}"
 
 echo "---------- utils.sh -----------"
 echo "APP_NAME=\"${APP_NAME}\""
@@ -13,53 +16,27 @@ echo "BINARY_NAME=\"${BINARY_NAME}\""
 echo "GH_REPO_PATH=\"${GH_REPO_PATH}\""
 echo "ORG_NAME=\"${ORG_NAME}\""
 
-# All common functions can be added to this file
-
-apply_patch() {
-  if [[ -z "$2" ]]; then
-    echo applying patch: "$1";
-  fi
-  # grep '^+++' "$1"  | sed -e 's#+++ [ab]/#./vscode/#' | while read line; do shasum -a 256 "${line}"; done
-
-  cp $1{,.bak}
-
-  replace "s|!!APP_NAME!!|${APP_NAME}|g" "$1"
-  replace "s|!!APP_NAME_LC!!|${APP_NAME_LC}|g" "$1"
-  replace "s|!!BINARY_NAME!!|${BINARY_NAME}|g" "$1"
-  replace "s|!!GH_REPO_PATH!!|${GH_REPO_PATH}|g" "$1"
-  replace "s|!!ORG_NAME!!|${ORG_NAME}|g" "$1"
-  replace "s|!!RELEASE_VERSION!!|${RELEASE_VERSION}|g" "$1"
-
-  if ! git apply --ignore-whitespace "$1"; then
-    echo failed to apply patch "$1" >&2
-    exit 1
-  fi
-
-  mv -f $1{.bak,}
+# Check if GNU sed is available
+exists() {
+  type -t "$1" > /dev/null 2>&1
 }
-
-exists() { type -t "$1" &> /dev/null; }
 
 is_gnu_sed() {
-  sed --version &> /dev/null
+  sed --version >/dev/null 2>&1
 }
 
-replace() {
-  if is_gnu_sed; then
-    sed -i -E "${1}" "${2}"
-  else
-    sed -i '' -E "${1}" "${2}"
-  fi
-}
-
-if ! exists gsed; then
-  if is_gnu_sed; then
-    function gsed() {
-      sed -i -E "$@"
-    }
-  else
-    function gsed() {
-      sed -i '' -E "$@"
-    }
-  fi
+# Use gsed if available (macOS), otherwise regular sed
+if exists gsed; then
+  REPLACE="gsed"
+elif is_gnu_sed; then
+  REPLACE="sed"
+else
+  REPLACE="sed"
 fi
+
+export APP_NAME
+export APP_NAME_LC
+export BINARY_NAME
+export GH_REPO_PATH
+export ORG_NAME
+export REPLACE

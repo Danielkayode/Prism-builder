2ee4c234c3e836c20d4900fcaeb7ed7552b5101d
Danielkayode <153762458+Danielkayode@users.noreply.github.com>
2026-01-08 20:50:28 +0100

    Update update_settings.sh

--- update_settings.sh
+++ update_settings.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/env bash
 # shellcheck disable=SC1091,2148
 
+# Define constants for settings search
 DEFAULT_TRUE="'default': true"
 DEFAULT_FALSE="'default': false"
 DEFAULT_ON="'default': TelemetryConfiguration.ON"
 DEFAULT_OFF="'default': TelemetryConfiguration.OFF"
 TELEMETRY_CRASH_REPORTER="'telemetry.enableCrashReporter':"
 TELEMETRY_CONFIGURATION=" TelemetryConfiguration.ON"
-NLS=workbench.settings.enableNaturalLanguageSearch
+NLS="workbench.settings.enableNaturalLanguageSearch"
 
 # include common functions
 . ../utils.sh
@@ -22,6 +24,7 @@ update_setting () {
 
   SETTING="${1}"
   LINE_NUM=0
+  # Search for the line where the setting is defined
   while read -r line; do
     LINE_NUM=$(( LINE_NUM + 1 ))
     if [[ "${line}" == *"${SETTING}"* ]]; then
@@ -34,10 +37,11 @@ update_setting () {
   done < "${FILENAME}"
 
   if [[ "${FOUND}" != "1" ]]; then
-    echo "${DEFAULT_TRUE} not found for setting ${SETTING} in file ${FILENAME}"
+    echo "Default value not found for setting ${SETTING} in ${FILENAME}"
     return
   fi
 
+  # Replace the found line with the disabled version
   if [[ "${line}" == *"${DEFAULT_TRUE}"* ]]; then
     DEFAULT_TRUE_TO_FALSE="${LINE_NUM}s/${DEFAULT_TRUE}/${DEFAULT_FALSE}/"
   else
@@ -47,15 +51,20 @@ update_setting () {
   replace "${DEFAULT_TRUE_TO_FALSE}" "${FILENAME}"
 }
 
+# Apply standard telemetry updates
 update_setting "${TELEMETRY_CRASH_REPORTER}" src/vs/workbench/electron-sandbox/desktop.contribution.ts
 update_setting "${TELEMETRY_CONFIGURATION}" src/vs/platform/telemetry/common/telemetryService.ts
 update_setting "${NLS}" src/vs/workbench/contrib/preferences/common/preferencesContribution.ts
 
-# --- ADDED: Inject serverDownloadUrlTemplate ---
-# This replaces the failing add-remote-url.patch
+# --- INJECT REMOTE DOWNLOAD URL ---
+# This replaces the failing add-remote-url.patch file
 echo "Injecting serverDownloadUrlTemplate into Gulp files..."
-URL_TEMPLATE="https://github.com/${GH_REPO_PATH}/releases/download/${RELEASE_VERSION}/${APP_NAME_LC}-reh-\${os}-\${arch}-${RELEASE_VERSION}.tar.gz"
 
-# Use @ as a delimiter for sed to avoid clashing with the forward slashes in the URL
-sed -i "s@version }))@version, serverDownloadUrlTemplate: '${URL_TEMPLATE}' })@g" build/gulpfile.reh.js
-sed -i "s@version }))@version, serverDownloadUrlTemplate: '${URL_TEMPLATE}' })@g" build/gulpfile.vscode.js
+# Construct the URL using environment variables
+URL="https://github.com/${GH_REPO_PATH}/releases/download/${RELEASE_VERSION}/${APP_NAME_LC}-reh-\${os}-\${arch}-${RELEASE_VERSION}.tar.gz"
+
+# We use '@' as a separator for sed because the URL contains forward slashes
+sed -i "s@version }))@version, serverDownloadUrlTemplate: '${URL}' })@g" build/gulpfile.reh.js
+sed -i "s@version }))@version, serverDownloadUrlTemplate: '${URL}' })@g" build/gulpfile.vscode.js
+
+echo "Update settings completed successfully."
